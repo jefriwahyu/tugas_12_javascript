caa3f4bdde4df7b88866093b98b4a85b2cdf1773
Jefriwahyu <memberjefry1@gmail.com>
2022-03-08 10:32:57 +0700

    Tugas 12 Javascript
    Tugas ini saya kumpulkan untuk NIOMIC.

---
+++ index.js
@@ -0,0 +1,10 @@
+function namaBuah(){
+  var buah = ['Pisang', 'Jeruk', 'Apel', 'Mangga']
+  console.log(buah)
+  buah.pop()
+  console.log(buah)
+  buah.shift()
+  return buah
+}
+
+console.log(namaBuah())
